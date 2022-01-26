5266b3553c9497c6c8c5be20d7d0157a4d777f09
Zwothe Siwada <82267759+Zwothe-Siwada@users.noreply.github.com>
2022-01-26 12:10:21 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zwo-calculator';
+  num1:number = 0;
+  num2:number = 0;
+  results:any = 0;
+
+  operators(symbol: string){
+    this.results = this.num1 + this.num2
+
+    if (this.num1/this.num2){
+      this.results = "Can't Divivde by 0"
+    }
+
+    if (symbol == '+'){
+      this.results = this.num1 + this.num2  
+    }
+    else if (symbol == '-'){
+      this.results = this.num1 - this.num2
+    }
+    else if ((symbol == '/') &&(this.num2!==0)){
+      this.results = this.num1 / this.num2
+    }
+    else if (symbol == '*'){
+      this.results = this.num1 * this.num2
+    }
+  }
 }
